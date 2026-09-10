748084aa01ef4ad1c5bca7330e209a41541f0e0c
Mayank Peswani <peswani.mayank11@gmail.com>
2026-09-10 15:56:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void insertionSort(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            int ke = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+};
