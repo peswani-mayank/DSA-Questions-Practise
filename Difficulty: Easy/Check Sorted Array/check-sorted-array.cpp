a1c4e625ea11e4058f3c9ded664811f737b94140
Mayank Peswani <peswani.mayank11@gmail.com>
2026-09-11 20:15:30 +0530

    Update check-sorted-array.cpp

--- Difficulty: Easy/Check Sorted Array/check-sorted-array.cpp
+++ Difficulty: Easy/Check Sorted Array/check-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
   public:
-    bool isSorted(vector<int>& arr) {
-        // code here
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]<arr[i-1])
-            {
-                return false;
+    bool isSorted(vector<int>& arr) {       // function check karega ki array sorted hai ya nahi
+
+        for(int i = 1; i < arr.size(); i++) {  // second element se start karke last element tak jayenge
+
+            if(arr[i] < arr[i - 1]) {        // agar current element previous element se chhota hai
+
+                return false;                // array sorted nahi hai, isliye false return karo
             }
-            
-            
         }
-        return true;
+
+        return true;                         // poora array check ho gaya aur koi problem nahi mili
     }
 };
