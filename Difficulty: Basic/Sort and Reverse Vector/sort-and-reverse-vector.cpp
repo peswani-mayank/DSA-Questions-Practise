4907e5bc8f67f6ad99d2e0be5ace15e5125e2596
Mayank Peswani <peswani.mayank11@gmail.com>
2026-09-23 23:47:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort and Reverse Vector/sort-and-reverse-vector.cpp
@@ -0,0 +1,17 @@
+// Function to sort the given vector
+vector<int> sortVector(vector<int> v) {
+    // code here
+    sort(v.begin(),v.end());
+    return v;
+    
+}
+
+// Function to reverse the order of elements
+vector<int> reverseVector(vector<int> v) {
+    reverse(v.begin(),v.end());
+    
+    return v;
+
+    
+    // code here
+}
