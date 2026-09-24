6206e52973e3c00e593f307419f390d0a5001198
Mayank Peswani <peswani.mayank11@gmail.com>
2026-09-24 18:23:51 +0530

    Update set-operations.cpp

--- Difficulty: Basic/Set Operations/set-operations.cpp
+++ Difficulty: Basic/Set Operations/set-operations.cpp
@@ -20,7 +20,11 @@ void setDisplay(set<int>& s) {
     
 void setErase(set<int>& s, int x) {
     // code here
-    if(s.find(x)!=s.end()){
+    if(s.find(x)!=s.end()){ //iska mtlb ye h ki s.end() wo last element ko nhi show krta h wo last element s 
+    //ek aage posiiton ko show krta h
+    //Eg.: 1   2   4   7   8   9   END
+    //to ye find krega pure m x ko agar array m x hoga to wo end 
+    //tk nhi phuchega or agar x tk phuch gya to iska mtlb ye h ki x us array m nhi h 
         s.erase(x);
         cout<<"erased "<<x<<endl;
     }else{
