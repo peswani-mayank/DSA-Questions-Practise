33b2a07a7222487a464f5f3a2d6c10f9e695f394
Mayank Peswani <peswani.mayank11@gmail.com>
2026-09-24 18:56:26 +0530

    Update reverse-array-using-stack.cpp

--- Difficulty: Basic/Reverse Array Using Stack/reverse-array-using-stack.cpp
+++ Difficulty: Basic/Reverse Array Using Stack/reverse-array-using-stack.cpp
@@ -9,6 +9,24 @@ class Solution {
         
         for(int i=0;i<arr.size();i++){
             arr[i]=s.top();
+            //i = 0
+            //   arr[0] = 5
+            //   pop 5
+            //   i = 1
+            //   arr[1] = 4
+            //   pop 4
+
+            //   i = 2
+            //   arr[2] = 3
+            //   pop 3
+
+            //   i = 3
+            //   arr[3] = 2
+            //   pop 2
+
+            //   i = 4
+            //   arr[4] = 1
+            //   pop 1
             s.pop();
         }
     }
