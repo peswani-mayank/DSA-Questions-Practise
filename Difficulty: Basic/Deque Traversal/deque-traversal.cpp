03d2b5110a906cb2611756918e5adf8db6089998
Mayank Peswani <peswani.mayank11@gmail.com>
2026-09-24 18:27:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Deque Traversal/deque-traversal.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    void printDeque(deque<int> &dq) {
+        // code here
+        for(int x:dq){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+};
